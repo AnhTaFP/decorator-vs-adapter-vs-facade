5afc1972735f1efefe389e9960fd3ac989f5de2a
t.viet <ta.viet@foodpanda.com>
2024-01-30 09:30:54 +0700

    Add more comments

--- main.go
+++ main.go
@@ -7,29 +7,31 @@ import (
 )
 
 func main() {
+	// standard sword
 	baseSword := weapon.Sword{
 		BaseDamage: 10,
 	}
 
 	swordEnhancer := weapon.NewEnhancer(&baseSword)
 	excalibur := swordEnhancer.Enhance(
-		weapon.WithFire(3),
-		weapon.WithPoison(1),
-		weapon.WithLight(10),
+		weapon.WithFire(3),   // inserts a fire gemstone
+		weapon.WithPoison(1), // puts a skill point in poison mastery
+		weapon.WithLight(10), // finds a holy shrine and imbues the sword with power of light
 	)
 
 	fmt.Printf("sword now does %d damage\n", excalibur.Damage())
 
+	// standard claymore
 	claymore := weapon.Sword{
 		BaseDamage: 1,
 	}
 
 	claymoreEnhancer := weapon.NewEnhancer(&claymore)
 	enhancedClaymore := claymoreEnhancer.Enhance(
-		weapon.WithSlash(10),
-		weapon.WithThunder(5),
-		weapon.WithPierce(10),
-		weapon.WithDark(5),
+		weapon.WithSlash(10),  // puts a skill point in slashing mastery
+		weapon.WithThunder(5), // inserts a thunder gemstone
+		weapon.WithPierce(10), // puts a skill point in piercing master
+		weapon.WithDark(5),    // inserts a dark gemstone
 	)
 
 	fmt.Printf("claymore now does %d damage\n", enhancedClaymore.Damage())
